ba93e1280647fe711d77e9ba42fc0ebce6f3f3eb
alejoevilches <alejoevilches@gmail.com>
2024-03-26 18:42:52 -0300

    add animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import './App.css'
 import { QuestionBox } from './components/QuestionBox'
 import {Questions} from "./components/Questions"
+import {Fade} from "react-awesome-reveal"
 
 function App() {
   return (
     <main>
       <nav className='nav'>Anonymous Questions App</nav>
-      <QuestionBox />
-      <Questions />
+      <Fade duration={3000}>
+        <QuestionBox />
+        <Fade delay={1000}>
+          <Questions />
+        </Fade>
+      </Fade>
     </main>
   )
 }
