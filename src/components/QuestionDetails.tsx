8428c5abe95e32ae8cc2b3cf49c2c2d54aa4e9d7
alejoevilches <alejoevilches@gmail.com>
2024-03-27 20:26:48 -0300

    add copy feature

--- src/components/QuestionDetails.tsx
+++ src/components/QuestionDetails.tsx
@@ -1,11 +1,12 @@
 import "./QuestionDetails.css"
 import { useQuestionsStore } from "../store/useQuestionsStore";
 import { Link } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import * as htmlToImage from 'html-to-image';
 
 export function QuestionDetails(){
   const {questions}=useQuestionsStore()
+  const [isCopied, setIsCopied]=useState(false)
 
   const getQuestionFromId=()=>{
     const currentUrl=window.location.href
@@ -13,22 +14,30 @@ export function QuestionDetails(){
     return questions.find((q)=>q.id===id)
   }
 
-  const q=getQuestionFromId()
+  const copyQuestionImageToClipboard = () => {
+    const questionDetailsNode = document.querySelector(".question-details-container");
+  
+    htmlToImage.toBlob(questionDetailsNode as HTMLElement, {height:200})
+      .then(function (blob) {
+        if (blob !== null) {
+          // Copiar el blob de la imagen al portapapeles
+          navigator.clipboard.write([new ClipboardItem({ 'image/png': blob })])
+            .then(function () {
+              console.log("La pregunta se ha copiado correctamente al portapapeles.");
+            }).catch(function (error) {
+              console.error("Error al copiar la pregunta al portapapeles:", error);
+            });
+            setIsCopied(true);
+        }
+      })
+      .catch(function (error) {
+        console.error("Ocurrió un error al generar la imagen:", error);
+      });
+  };
+  
+  const q = getQuestionFromId();
 
   useEffect(()=>{
-    const imgNode=document.querySelector(".question-details-container")
-
-  htmlToImage.toPng(imgNode as HTMLElement, {height:200, width:500})
-    .then(function (dataUrl) {
-      const img = new Image();
-      img.src = dataUrl;
-      const ogImageMetaTag=document.querySelector('meta[property="og:image"]');
-      ogImageMetaTag?.setAttribute("content", dataUrl);
-    })
-    .catch(function (error) {
-      console.error('oops, something went wrong!', error);
-  });
-
     const ogTitleMetaTag = document.querySelector('meta[property="og:title"]');
     const ogDescriptionMetaTag = document.querySelector('meta[property="og:description"]');
     if (ogTitleMetaTag && q){
@@ -47,6 +56,7 @@ export function QuestionDetails(){
       <Link to={"/"}>
         <button className="back-button">Volver al inicio</button>
       </Link>
+      <button className="back-button copy-button" onClick={copyQuestionImageToClipboard}>{isCopied ? "Copiado!" : "Copiar pregunta en el portapapeles"}</button>
     </main>
   )
 }
