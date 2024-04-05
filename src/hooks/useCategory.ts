799d01e3c54c808d0978dcd0dbb876f4f8b5f2fc
alejoevilches <alejoevilches@gmail.com>
2024-04-05 16:44:40 -0300

    fix categories issue on mobile

--- src/hooks/useCategory.ts
+++ src/hooks/useCategory.ts
@@ -3,6 +3,8 @@ import { categories } from "../utils/constants"
 
 export const useCategory=()=>{
   const [category, setCategory]=useState<string>()
+
+  //Chequea si el usuario está en alguna categoría segun la ruta a la que accede
   const checkCategory = () => {
     const urlArray = window.location.href.split("/");
     const foundCategory = categories.find(cat => urlArray.includes(cat));
